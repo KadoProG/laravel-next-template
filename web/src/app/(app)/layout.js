5ce8422a883f38a1ebaca0799fc13a4751a659a0
KadoProG <splatrain@gmail.com>
2024-04-25 14:25:19 +0900

    prettierを適用

--- web/src/app/(app)/layout.js
+++ web/src/app/(app)/layout.js
@@ -1,23 +1,23 @@
-'use client'
+'use client';
 
-import { useAuth } from '@/hooks/auth'
-import Navigation from '@/app/(app)/Navigation'
-import Loading from '@/app/(app)/Loading'
+import { useAuth } from '@/hooks/auth';
+import Navigation from '@/app/(app)/Navigation';
+import Loading from '@/app/(app)/Loading';
 
 const AppLayout = ({ children, header }) => {
-    const { user } = useAuth({ middleware: 'auth' })
+  const { user } = useAuth({ middleware: 'auth' });
 
-    if (!user) {
-        return <Loading />
-    }
+  if (!user) {
+    return <Loading />;
+  }
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <Navigation user={user} />
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navigation user={user} />
 
-            <main>{children}</main>
-        </div>
-    )
-}
+      <main>{children}</main>
+    </div>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
